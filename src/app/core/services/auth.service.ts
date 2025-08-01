3a308e4f3a3f1379205631600c667deb2f2280da
Silviu Hermeneanu <silh6767@gmail.com>
2025-08-02 00:31:08 +0300

    fix: Add missing model imports to services

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../models/user.model';
---
+++ src/app/core/services/quiz.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Quiz, QuizAttempt, QuizResult, Question } from '../models/quiz.model';
